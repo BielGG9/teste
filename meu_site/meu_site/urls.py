a27315329826b2f9169635cd5b3de8ccdaca10d7
Nicollas1305 <cartola1320@gmail.com>
2024-11-07 21:26:43 -0300

    correction in commit

--- meu_site/meu_site/urls.py
+++ meu_site/meu_site/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('principal/', include('principal.urls')),
+    path('', include('principal.urls')),
 ]
